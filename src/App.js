3acf497b8ff8add8015d5ddf87cc1824d5764245
Hafeefa <hafeefa40@gmail.com>
2023-08-09 18:29:58 -0400

    practicing with styling and d3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, NavLink, Routes } from 'react-router-dom';
+import LogoutIcon from '@mui/icons-material/Logout';
+import SideBar from './components/SideBar';
+import Home from './pages/Home';
 
 function App() {
+  // will put this somewhere else later
+  const activeMenu = false;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className='flex relative dark: bg-main-dark-bg'>
+          <div className='fixed right-4 top-4 hover: drop-shadow-xl' style={{ zIndex: "1000" }}>
+            {/* put icon here */}
+            <button type="button">
+              <LogoutIcon className="text-3xl hover:drop-shadow-xl hover:bg-light-gray text-purple-700" />
+            </button>
+          </div>
+          {activeMenu ? (
+            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+              <SideBar></SideBar>
+            </div>
+          ) : (
+            <div className='w-0'>
+              {/* sidebar =false */}
+            </div>
+          )}
+
+          <div className={activeMenu ? "md:ml-72 w-full" : "w-full flex-2"}>
+            <div className='fixed md:static bg-main-bg navbar w-full'>
+              Navbar
+            </div>
+          </div>
+        </div>
+
+          <Routes>
+            {/* Dashboard */}
+            <Route path="/" element={<Home/>} />
+
+            {/* components */}
+            <Route path="sidebar" element={<SideBar/>}/>
+          </Routes>
+      </Router>
+
     </div>
   );
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Home from '../pages/Home';
+
+function SideBar() {
+  const activeMenu = true;
+
+  return (
+    <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
+      {activeMenu && (
+        <div className='flex justify-between items-center'>
+          <Link to="/" onClick={() => { }} className="item-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-right">
+             <span> shoppy </span>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SideBar;
