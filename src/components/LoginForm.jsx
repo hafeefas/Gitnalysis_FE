168599071d32c26476e9711889ded7e8de23e4c7
Jonathan Shabatayev <JONATHAN.SHABATAYEV68@bcmail.cuny.edu>
2023-08-11 16:51:23 -0400

    Add styling for login box

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,39 +1,39 @@
 import React from 'react';
 import GitHubLogo from '../icons/github-logo.png';
+import '../login.css';
 
 function LoginForm() {
   return (
-    <div>
-      <div>
-        <h2>GitHub Login</h2>
-        
-        <form>
-          <div>
-            <label htmlFor="username">GitHub Username</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-            />
-          </div>
-
-          <div>
-            <label htmlFor="repository">Repository Name</label>
-            <input
-              type="text"
-              id="repository"
-              name="repository"
-            />
-          </div>
-
-          <button>
-            <img src={GitHubLogo} alt="GitHub Logo" /> Login with GitHub
-          </button>
-        </form>
+    <form className="space-y-4">
+      <div className="mb-4">
+        <label htmlFor="username" className="block mb-2 text-sm font-medium text-gray-600">GitHub Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          className="w-full p-2 border rounded focus:outline-none focus:border-purple-500"
+        />
       </div>
-    </div>
+
+      <div className="mb-4">
+        <label htmlFor="repository" className="block mb-2 text-sm font-medium text-gray-600">Repository Name</label>
+        <input
+          type="text"
+          id="repository"
+          name="repository"
+          className="w-full p-2 border rounded focus:outline-none focus:border-purple-500"
+        />
+      </div>
+
+      <button className="w-full bg-purple-500 hover:bg-purple-600 text-white py-2 rounded focus:outline-none">
+        Submit
+      </button>
+
+      <button className="w-full flex justify-center items-center bg-gray-800 hover:bg-gray-900 text-white py-2 rounded focus:outline-none">
+        <img src={GitHubLogo} alt="GitHub Logo" className="h-5 w-5 mr-2" /> Login with GitHub
+      </button>
+    </form>
   );
 }
 
 export default LoginForm;
-
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm';
+import '../login.css';
 
 function Login() {
   return (
-    <div className="flex">
-      <LoginForm />
+    <div className="min-h-screen flex justify-center items-center bg-gray-200">
+      <div className="bg-gray-400 p-6 rounded-lg shadow-md w-96">
+        <LoginForm />
+      </div>
     </div>
   );
 }
