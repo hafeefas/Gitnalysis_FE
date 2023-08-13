c59244f2bd66ab9a1e583764dff7286a93e5f6e5
Jonathan Shabatayev <JONATHAN.SHABATAYEV68@bcmail.cuny.edu>
2023-08-12 20:43:54 -0400

    Add start date and end date range

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -2,24 +2,45 @@ import React from 'react';
 import { Toolbar } from '@material-ui/core';
 
 function DateRangeToolbar() {
-    return (
-        <Toolbar>
-            <FormControl>
-                <InputLabel id="time-range-label">Time Range</InputLabel>
-                <Select
-                    labelId="time-range-label"
-                    id="time-range-select"
-                    value=
-                    onChange=
-                >
-                    <MenuItem value="daily">Daily</MenuItem>
-                    <MenuItem value="weekly">Weekly</MenuItem>
-                    <MenuItem value="monthly">Monthly</MenuItem>
-                    <MenuItem value="yearly">Yearly</MenuItem>
-                </Select>
-            </FormControl>
-        </Toolbar>
-    );
+return (
+    <Toolbar>
+      <FormControl style={{ marginRight: '20px' }}>
+        <InputLabel id="time-range-label">Time Range</InputLabel>
+        <Select
+          labelId="time-range-label"
+          id="time-range-select"
+          value={timeRange}
+          onChange={(e) => setTimeRange(e.target.value)}
+        >
+          <MenuItem value="daily">Daily</MenuItem>
+          <MenuItem value="weekly">Weekly</MenuItem>
+          <MenuItem value="monthly">Monthly</MenuItem>
+          <MenuItem value="yearly">Yearly</MenuItem>
+        </Select>
+      </FormControl>
+      <TextField
+        id="start-date-picker"
+        label="Start Date"
+        type="date"
+        value={startDate.toISOString().split('T')[0]}
+        onChange={(e) => setStartDate(new Date(e.target.value))}
+        style={{ marginRight: '10px' }}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <TextField
+        id="end-date-picker"
+        label="End Date"
+        type="date"
+        value={endDate.toISOString().split('T')[0]}
+        onChange={(e) => setEndDate(new Date(e.target.value))}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </Toolbar>
+  );
 }
 
 export default DateRangeToolbar;
