ac29a6da7c3eace8cfbb4d39f4f0a2c836b080ed
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-08-12 20:57:14 -0400

    make a repos page

---
+++ src/pages/Repos.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import RepoList from '../components/RepoList';
+import '../login.css';
+
+const Repos = ({repos, setCurrRepo}) => {
+  return (
+
+    <div className="min-h-screen flex justify-center items-center bg-gray-200">
+      <div className="bg-gray-400 p-6 rounded-lg shadow-md w-96">
+        <RepoList repos={repos} setCurrRepo={setCurrRepo} />
+      </div>
+    </div>
+  );
+}
+
+export default Repos
