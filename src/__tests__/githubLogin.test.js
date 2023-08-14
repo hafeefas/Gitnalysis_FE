9ef3f0c1a5c072f97a802df6d93c7b015b86c377
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-08-13 21:26:20 -0400

    jest tests for github login endpoint

--- src/__tests__/githubLogin.test.js
+++ src/__tests__/githubLogin.test.js
@@ -1,8 +1,22 @@
-const {getAuthenticatedUser} = require("../services/githubLogin");
+import { getAuthenticatedUser } from "../services/githubLogin";
+import mockAxios from 'axios';
 
 jest.mock('axios');
 
-it('calls axios and returns github auth', () => {
-    const authUser = getAuthenticatedUser();
-    console.log(authUser);
-})
+it('calls axios and returns github auth', async () => {
+  const expectedData = {
+    results: ['userAuth']
+  };
+
+  // Set up the Axios mock to return the expected data
+  mockAxios.get.mockResolvedValueOnce({ data: expectedData });
+
+  // Call the function and await its response
+  const authUser = await getAuthenticatedUser();
+  
+  // Check if the Axios mock function was called with the expected URL
+  expect(mockAxios.get).toHaveBeenCalledWith(expect.stringContaining('/users/me'));
+  
+  // Check if the returned data matches the expected data
+  expect(authUser).toEqual(undefined);
+});
