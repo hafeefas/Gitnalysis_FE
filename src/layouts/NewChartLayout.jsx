280d3f2e7a77cb8611b652042df82858a0105c6c
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-08-16 17:21:00 -0400

    create new chart layout for 11 components

--- src/layouts/NewChartLayout.jsx
+++ src/layouts/NewChartLayout.jsx
@@ -25,48 +25,48 @@ const NewChartLayout = ({username,currRepo}) => {
   console.log("Rendering NewChartLayout");
 
   return (
-    <><div>
-          <div className="flex h-auto gap-y-12 p-10">
+    <><div className="w-screen h-full" style={{backgroundColor:'red'}}>
+          <div className="flex gap-y-12 p-10">
               <div className="flex w-full">
-                  <div className="grid grid-cols-3 grid-rows-2 gap-x-4 pr-4 text-center" style={{width: "49%"}}>
-                      <div className="flex col-span-1 h-24 bg-slate-300 p-2 items-center justify-center">Collaborators</div>
-                      <div className="flex col-span-1 h-24 bg-slate-300 p-2 items-center justify-center">Branches</div>
-                      <div className="flex col-span-1 h-24 bg-slate-300 p-2 items-center justify-center">Pull Request Merged Success Rate</div>
+                  <div className="grid grid-cols-3 grid-rows-2 gap-x-4 pr-4 text-center" style={{width: "54%"}}>
+                      <div className="flex col-span-1 h-32 bg-slate-300 p-2 items-center justify-center rounded-xl">Collaborators</div>
+                      <div className="flex col-span-1 h-32 bg-slate-300 p-2 items-center justify-center rounded-xl">Branches</div>
+                      <div className="flex col-span-1 h-32 bg-slate-300 p-2 items-center justify-center rounded-xl">Pull Request Merged Success Rate</div>
                   </div>
                   <div className="w-1/3">
                       <div className="grid grid-cols-2 gap-x-4 gap-y-4 text-center">
-                          <div className=" bg-slate-300" style={{width: "77%"}}>
+                          <div className="bg-slate-300 ml-1 w-4/5 rounded-xl">
     
-                              <div className="flex h-20 p-2 items-center justify-center">Merged PRs</div>
+                              <div className="flex h-28 p-2 items-center justify-center">Merged PRs</div>
                           </div>
-                          <div className=" bg-slate-300" style={{width: "77%", transform: "translateX(-50px)"}}>
+                          <div className="bg-slate-300 ml-1 w-4/5 rounded-xl" style={{transform: "translateX(-50px)"}}>
                 
-                              <div className="flex h-20 p-2 items-center justify-center">Open PRs</div>
+                              <div className="flex h-28 p-2 items-center justify-center">Open PRs</div>
                           </div>
-                          <div className="bg-slate-300" style={{width: "77%"}}>
+                          <div className="bg-slate-300 ml-1 w-4/5 rounded-xl">
         
-                              <div className="flex h-20 p-2 items-center justify-center">Closed Issues</div>
+                              <div className="flex p-2 items-center justify-center h-28">Closed Issues</div>
                           </div>
-                          <div className="bg-slate-300" style={{ width: '77%', transform: 'translateX(-50px)' }}>
+                          <div className="bg-slate-300 ml-1 w-4/5 rounded-xl" style={{transform: 'translateX(-50px)' }}>
                 
-                              <div className="flex h-20 p-2 items-center justify-center">New Issues</div>
+                              <div className="flex h-28 p-2 items-center justify-center">New Issues</div>
                           </div>
                       </div>
                   </div>
               </div>
           </div>
-          <div className="pl-10 text-center" style={{transform:'translateY(-120px)'}}>
-              <div className="flex bg-slate-300 h-48 items-center justify-center" style={{width: "46.5%"}} >
-              <div className="flex h-20 p-2 items-center justify-center object-fill" style={{width: "106.5%"}}>lead time</div>
+          <div className="pl-10 text-center" style={{transform:'translateY(-150px)'}}>
+              <div className="flex bg-slate-300 items-center justify-center rounded-xl" style={{width: "51.5%"}} >
+              <div className="flex h-56 p-2 items-center justify-center object-fill" style={{width: "136.5%"}}>lead time</div>
               </div>
           </div>
       </div><div className="flex">
-              <div className="pl-10 grid-rows-12 grid grid-cols-2 gap-x-4 gap-y-4 text-center" style={{transform: "translateY(-96px)",width: "208.5%"}}>
-                  <div className="flex col-span-1 h-48 bg-slate-300 p-2 items-center justify-center">Change Failure Rate('CFR')</div>
-                  <div className="flex col-span-1 h-48 bg-slate-300 p-2 justify-center items-center">Issues</div>
+              <div className="grid grid-cols-2 gap-x-4 gap-y-4 text-center ml-10 rounded-xl" style={{transform: "translateY(-335px)",width: "50%"}}>
+                  <div className="flex col-span-1 h-60 bg-slate-300 p-2 items-center justify-center rounded-xl">Change Failure Rate('CFR')</div>
+                  <div className="flex col-span-1 h-60 bg-slate-300 p-2 justify-center items-center rounded-xl">Issues</div>
               </div>
-              <div className="pl-5 grid-rows-12 grid grid-cols-2 gap-x-4 gap-y-4 text-center" style={{transform: "translate(-10px,-230px)", width:"230%"}}>
-                  <div className="flex bg-slate-300 p-2 justify-center items-center" style={{height:"328px", transform: "translateY(-135px)"}}>Commits w/ collaborators barchart</div>
+              <div style={{transform: "translate(20px,-470px)"}}>
+                  <div className="flex bg-slate-300 p-2 justify-center items-center rounded-xl" style={{width: "140%", height:'375px'}}>Commits w/ collaborators barchart</div>
           </div>
 </div></>
 )
