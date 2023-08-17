6950f7fb28fc90b6de14bed5a1fd4a7d4550a6ef
Jonathan Shabatayev <JONATHAN.SHABATAYEV68@bcmail.cuny.edu>
2023-08-17 00:05:42 -0400

    Place new component on layout, send in the two props, and make the use states

---
+++ src/components/Branches.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const Branches = ({ username, repo }) => {
+    const [branches, setBranches] = useState([]);
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    return (
+    );
+}
+
+export default Branches;
--- src/services/githubLogin.js
+++ src/services/githubLogin.js
@@ -4,11 +4,11 @@ axios.defaults.withCredentials=true;
 // Modify the authenticateWithGitHub function to accept an additional 'repo' parameter
 export const getAuthenticatedUser = async () => {
     try {
-      const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/users/me`,
+      // const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/users/me`,
+      const response = await axios.get(`http://localhost:8080/api/users/me`,
       {},
       {withCredentials:true}
       );
-      // const response = await axios.get(`http://localhost:8080/api/users/me`);
       console.log('Authenticated User:', response.data);
     } catch (error) {
       console.error('Error fetching authenticated user:', error);
@@ -17,10 +17,10 @@ export const getAuthenticatedUser = async () => {
 
 export const getUserRepos = async () => {
   try {
-      const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/users/me/repos`,
-      {},
-      {withCredentials:true});
-      // const response = await axios.get(`http://localhost:8080/api/users/me/repos`);
+      // const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/users/me/repos`,
+    const response = await axios.get(`http://localhost:8080/api/users/me/repos`,
+    {},
+    {withCredentials:true});
       console.log('Authenticated User Repos:', response.data);
       //Creates an object of full_name name and repo.name
       const repoNames = response.data.map(repo => {return {full_name:repo.full_name,name:repo.name}});
@@ -33,7 +33,8 @@ export const getUserRepos = async () => {
 
 export const logoutGitHubUser = async () => {
   try {
-   const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/github/logout`);
+   // const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/github/logout`);
+    const response = await axios.get(`http://localhost:8080/api/users/me/repos`);
    console.log('Logout response:', response);
    } catch (error){
     console.error('Error logging out authenticated user:', error);
