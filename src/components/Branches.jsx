41abe2983dbbc3a470a65fd13da3e4391f64c06d
Jonathan Shabatayev <JONATHAN.SHABATAYEV68@bcmail.cuny.edu>
2023-08-17 00:34:47 -0400

    Add use effect that gets branches from backend using props

--- src/components/Branches.jsx
+++ src/components/Branches.jsx
@@ -1,9 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Branches = ({ username, repo }) => {
     const [branches, setBranches] = useState([]);
     const [showDropdown, setShowDropdown] = useState(false);
+
+    useEffect(() => {
+        async function fetchBranches() {
+            try {
+                const response = await axios.get(`http://localhost:8080/api/branches/${username}/${repo}`);
+                setBranches(response.data.branches);
+            } catch (error) {
+                console.error('Error fetching branches:', error);
+            }
+        }
+
+        fetchBranches();
+    }, [username, repo]);
 
     return (
     );
