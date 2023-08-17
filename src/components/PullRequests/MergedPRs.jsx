8abf5afac81d6ac1b8533014d6826a59051f9023
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-08-17 19:50:18 -0400

    Remove hover from closed PR display

--- src/components/PullRequests/MergedPRs.jsx
+++ src/components/PullRequests/MergedPRs.jsx
@@ -1,60 +1,42 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+const MergedPRCount = ({ fullRepo }) => {
+  const [closedPrs, setClosedPrs] = useState(null);
 
-const MergedPRCount = ({fullRepo}) => {
-    const [mergedPr, setMergedPr] = useState(0)
-    const [closedPrs, setClosedPrs] = useState(null);
-    const [isHovered, setIsHovered] = useState(false);
-    
-
-    useEffect(() => {
-        async function mergedPullRequests (){
-            try {
-                if (typeof fullRepo !== 'string') {
-                    console.error('fullRepo should be a string');
-                    return;
-                }
-                console.log("repoINfo,", fullRepo)
-                const repoParts = fullRepo.split('/')
-                const username = repoParts[0];
-                console.log(username)
-                const repo = repoParts[1];
-                        
-                const response = await axios.get(`http://localhost:8080/api/pull-requests/merge-success-rate/${username}/${repo}`)
-                console.log(response.data.mergeSuccessRate)
-                setMergedPr(response.data.mergeSuccessRate)
-
-                setClosedPrs(response.data.successfullyMergedPRs)
-                
-            } catch (error) {
-                console.log(error)
-            }
+  useEffect(() => {
+    async function mergedPullRequests() {
+      try {
+        if (typeof fullRepo !== "string") {
+          console.error("fullRepo should be a string");
+          return;
         }
-        mergedPullRequests()
-    }, [fullRepo])
+        console.log("repoINfo,", fullRepo);
+        const repoParts = fullRepo.split("/");
+        const username = repoParts[0];
+        console.log(username);
+        const repo = repoParts[1];
 
-    return(
-        <div 
-            className="flex flex-col justify-center h-full"
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-        >
-            {isHovered ? (
-                <>
-                <div style={{ color: 'lightpink' }}>Closed PRs</div>
-                {closedPrs !== null && <div className="text-xl">{closedPrs}</div>}
-            </>
-            ) : (
-                <>
-                    <div style={{ color: 'pink' }}>Merge Success Rate</div>
-                    {mergedPr !== null && <div className="text-xl">{mergedPr}%</div>}
-                </>
-            )}
-        </div>
-    );
-}
+        const response = await axios.get(
+          `http://localhost:8080/api/pull-requests/merge-success-rate/${username}/${repo}`
+        );
 
+        setClosedPrs(response.data.successfullyMergedPRs);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    mergedPullRequests();
+  }, [fullRepo]);
 
+  return (
+    <div className="flex flex-col justify-center h-full">
+      <>
+        <div style={{ color: "lightpink" }}>Closed PRs</div>
+        {closedPrs !== null && <div className="text-xl">{closedPrs}</div>}
+      </>
+    </div>
+  );
+};
 
-export default MergedPRCount
+export default MergedPRCount;
