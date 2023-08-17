b9740a74b71aef2bcf6747962859c511602309e9
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-08-17 18:56:38 -0400

    fill in under line and edit placement

--- src/components/LeadTimeChart.jsx
+++ src/components/LeadTimeChart.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react'
 import * as d3 from 'd3'
 
 const LeadTimeChart = ({repoInfo}) => {
-  const width = 400;
+  const width = 600;
   const height = 120;
   const padding = 20;
 
@@ -48,34 +48,6 @@ const LeadTimeChart = ({repoInfo}) => {
         return d.merged_at.getDate();
       })));
 
-       const createGradient = select => {
-    const gradient = select
-      .select('defs')
-        .append('linearGradient')
-          .attr('id', 'gradient')
-          .attr('gradientTransform', 'rotate(90)');
-  gradient
-  .append('stop')
-  .attr('stop-color', '#F2C66B')
-  .attr('offset', '0%');
-  
-  gradient
-  .append('stop')
-  .attr('stop-color', '#D13D73')
-  .attr('offset', '100%');
-
-    gradient
-      .append('stop')
-        .attr('offset', '0%')
-        .attr('style', 'stop-color:#BBF6CA;stop-opacity:0.05');
-
-    gradient
-      .append('stop')
-        .attr('offset', '100%')
-        .attr('style', 'stop-color:#BBF6CA;stop-opacity:.5');
-  }
-  
-
     // console.log(maxAverageInSeconds)
     //xscales
     const xScale = d3.scaleTime()
@@ -91,15 +63,13 @@ const LeadTimeChart = ({repoInfo}) => {
     return new Date(repoInfo.data?.find((d) => {
       return d.merged_at.getDate() === day;
     }).merged_at);
-  })).tickFormat(d3.timeFormat("%d"));
+  })).tickFormat(d3.timeFormat("%m/%d"));
   
     //customize y axis tick values to show less
     const yAxisTicks = yScale.ticks(5);
 
     // Define y-axis scale and axis
     const yAxis = d3.axisLeft(yScale).tickValues(yAxisTicks);
-
-    console.log(createGradient,'create gradient');
 
       // Render x-axis
     d3.select(svgRef.current)
@@ -112,74 +82,62 @@ const LeadTimeChart = ({repoInfo}) => {
     d3.select(svgRef.current)
     .append("g")
     .attr("class", "y-axis")
-    .attr("transform", `translate(${padding}, 0)`)
+    .attr("transform", `translate(0, 0)`)
     .call(yAxis);
-    
+
     //setup functions to drawlines
     const line = d3.line()
       .x(data => xScale(data.merged_at))
       .y(data => yScale(convertAverageTimeInSeconds(data.average)))
       .curve(d3.curveNatural);
 
-      const areaGenerator = d3
-    .area()
-    .x(data => xScale(data.merged_at))
-    .y0(yScale(0)) // The bottom of the area
-    .y1(data => yScale(convertAverageTimeInSeconds(data.average)))
-    .curve(d3.curveNatural);
-
-      // Select the 'path.area' element within svgRef.current
-  d3.select(svgRef.current)
-    .select('path.area') // Correctly select the path with class 'area'
-    .datum(repoInfo.data)
-    .attr('class', 'area')
-    .attr('d', areaGenerator)
-    .attr('fill', 'url(#gradient)') // Apply the gradient fill
-    .attr('stroke', 'white')
-    .attr('stroke-width', 5);
-
       // X-axis label
+    d3.select(svgRef.current)
+      .append("text")
+      .attr("class", "x-axis-label")
+      .attr("x", width / 2)
+      .attr("y", height - padding / 2 + 30)
+      .style("text-anchor", "middle")
+      .style("fill", "white")
+      .style("font-size", "12px")
+      .text("Merged At");
+
+    // Y-axis label
+    d3.select(svgRef.current)
+      .append("text")
+      .attr("class", "y-axis-label")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -height / 3 - 30)
+      .attr("y", padding / 3 - 40) 
+      .style("text-anchor", "middle")
+      .style("fill", "white")
+      .style("font-size", "12px")
+      .text("Average");
+
+// Create and render the area chart
+const areaGenerator = d3
+  .area()
+  .x((data) => xScale(data.merged_at))
+  .y0(yScale(0)) // Start at the bottom of the chart
+  .y1((data) => yScale(convertAverageTimeInSeconds(data.average)))
+  .curve(d3.curveNatural);
+
+// Render the area chart
 d3.select(svgRef.current)
-  .append("text")
-  .attr("class", "x-axis-label")
-  .attr("x", width / 2)
-  .attr("y", height - padding / 2 + 20)
-  .style("text-anchor", "middle")
-  .style("fill", "white")
-  .style("font-size", "12px")
-  .text("Merged At");
-
-// Y-axis label
-d3.select(svgRef.current)
-  .append("text")
-  .attr("class", "y-axis-label")
-  .attr("transform", "rotate(-90)")
-  .attr("x", -height / 3 - 30)
-  .attr("y", padding / 3 -20) 
-  .style("text-anchor", "middle")
-  .style("fill", "white")
-  .style("font-size", "12px")
-  .text("Average");
-      
-    //draw line
-    const path = d3.select(svgRef.current).select('path')
-    .attr('data', (value) => line(repoInfo.data))
-    .attr('fill','url(#gradient)')
-    .attr('stroke','#22E0E3ff')
-    .attr('stroke-width', 2);
-
-     // Generate path data for the line
-    const pathData = line(repoInfo.data);
-
-    // Set the 'd' attribute of the path element with the generated path data
-    path.attr('d', pathData);
+  .append("path")
+  .datum(repoInfo.data) // Pass the entire data array to the area generator
+  .attr("class", "area-chart")
+  .attr("d", areaGenerator) // Set the 'd' attribute to the generated area path
+  .attr("fill", "rgba(6, 182, 212, 0.1)") // Set the fill color
+  .attr("stroke", "#67e8f9") // Set the stroke color
+  .attr("stroke-width", 2); // Set the stroke width
   
   }
   },[repoInfo])
 return (
     <div className="flex w-full justify-center items-center">
-        <svg id="chart" ref={svgRef} viewBox="-50 -5 500 150">
-            <path d="" fill="url(#gradient)" stroke="white" strokeWidth="5"/>
+        <svg id="chart" ref={svgRef} viewBox="-60 8 700 150">
+            <path d="" fill="" stroke="white" strokeWidth="5"/>
         </svg>
     </div>
   )
