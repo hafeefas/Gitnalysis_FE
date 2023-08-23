32048fbe76f08da07357e4517c4e9e3389746b95
Andrea <Andrea.Tranchina82@gmail.com>
2023-08-23 00:34:48 -0400

    more fixes to tablet dashboard

--- src/layouts/TabletChartLayout.jsx
+++ src/layouts/TabletChartLayout.jsx
@@ -124,7 +124,7 @@ const TabletChartLayout = ({ username, currRepo }) => {
     //   </div>
     // </div>
     <div className="w-screen h-full overflow-scroll p-20 pb-24 pt-24 pr-0">
-      <div className="grid grid-cols-4 grid-rows-27 gap-4 pr-4 text-center ">
+      <div className="grid grid-cols-4 grid-rows-26 gap-4 pr-4 text-center ">
         <div className="flex col-span-2 h-32 p-2 iflex w-full justify-center items-center bg-gradient-to-br from-teal-300 to-sky-500 hover:from-pink-500 hover:to-yellow-500 rounded-xl font-bold text-white shadow-2xl">
           <Collaborators />
         </div>
@@ -132,7 +132,7 @@ const TabletChartLayout = ({ username, currRepo }) => {
           <Branches />
       </div>
       <div
-        className="col-span-5 row-span-2 col-start-1 row-start-2 text-center items-center rounded-xl shadow-3xl text-white h-full font-bold"
+        className="col-span-4 row-span-2 col-start-1 row-start-2 text-center items-center rounded-xl shadow-3xl text-white h-full font-bold"
         style={{ backgroundColor: "#171C2Eff" }}
       >
         Lead Time for Change
@@ -178,13 +178,13 @@ const TabletChartLayout = ({ username, currRepo }) => {
         <IssuesTimeline />
       </div>
       <div
-        className="col-span-4 row-span-3 col-start-1 row-start-10 ml-1 h-96 p-2 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
+        className="col-span-4 row-span-3 col-start-1 row-start-9 ml-1 h-96 p-2 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
         style={{ backgroundColor: "#171C2Eff" }}
       >
         <BarChart fullRepo={currRepo} />
       </div>
       <div
-        className="flex row-span-2 col-start-1 row-start-14 rounded-xl shadow-3xl ml-1 h-64 items-center justify-center font-bold text-white bg-gradient-to-br from-yellow-300 to-orange-500 hover:from-pink-500 hover:to-yellow-500 z-10 transition ease-in-out duration-200 hover:w-80 hover:relative hover:right-24 oveflow-scroll"
+        className="flex row-span-2 col-start-1 row-start-12 rounded-xl shadow-3xl ml-1 h-64 items-center justify-center font-bold text-white bg-gradient-to-br from-yellow-300 to-orange-500 hover:from-pink-500 hover:to-yellow-500 z-10 transition ease-in-out duration-200 hover:w-80 hover:relative hover:right-24 oveflow-scroll"
         style={{
           backgroundColor: "#171C2Eff",
           transition: "ease-in-out 500ms",
@@ -193,156 +193,47 @@ const TabletChartLayout = ({ username, currRepo }) => {
         <Forks />
       </div>
       <div
-        className="col-span-3 col-start-2 row-start-14 p-2 h-32 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white bg-gradient-to-br from-emerald-500 to-emerald-800"
+        className="col-span-3 col-start-2 row-start-12 p-2 h-32 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white bg-gradient-to-br from-emerald-500 to-emerald-800"
         style={{ backgroundColor: "#171C2Eff" }}
       >
         <Stargazers />
       </div>
       <div
-        className="col-span-3 col-start-2 row-start-15 p-2 h-32 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white bg-gradient-to-br from-blue-500 to-blue-900"
+        className="col-span-3 col-start-2 row-start-13 p-2 h-32 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white bg-gradient-to-br from-blue-500 to-blue-900"
         style={{ backgroundColor: "#171C2Eff" }}
       >
         <DateCreated />
       </div>
       <div
-        className="col-span-4 row-span-3 col-start-1 row-start-16 p-2 h-96 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
+        className="col-span-4 row-span-3 col-start-1 row-start-14 p-2 h-96 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
         style={{ backgroundColor: "#171C2Eff" }}
       >
         Change Failure Rate (CFR)
       </div>
       <div
-        className="col-span-4 row-span-6 col-start-1 row-start-20 p-2 h-192 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
+        className="col-span-4 row-span-6 col-start-1 row-start-17 p-2 h-192 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
         style={{ backgroundColor: "#171C2Eff" }}
       >
         <Activity />
       </div>
       <div
-        className="col-span-4 row-span-3 col-start-1 row-start-26 p-2 h-96 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
+        className="col-span-4 row-span-3 col-start-1 row-start-23 p-2 h-96 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
         style={{ backgroundColor: "#171C2Eff" }}
       >
         <MergeSuccessRatePie />
       </div>
       <div
-        className="col-start-2 row-start-30 p-2 h-32 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
+        className="col-start-2 row-start-26 p-2 h-32 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
         style={{ backgroundColor: "#171C2Eff" }}
       >
         <NumCommits />
       </div>
       <div
-        className="col-start-3 row-start-30 p-2 h-32 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
+        className="col-start-3 row-start-26 p-2 h-32 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
         style={{ backgroundColor: "#171C2Eff" }}
       >
         <NumDeployments />
       </div>
-      {/* <div className="flex col-span-2 h-32 p-2 iflex w-full justify-center items-center bg-gradient-to-br from-teal-300 to-sky-500 hover:from-pink-500 hover:to-yellow-500 rounded-xl font-bold text-white shadow-2xl">
-        <Collaborators />
-      </div>
-      <div className="flex col-span-2 col-start-3 h-32 p-2 iflex w-full justify-center items-center bg-gradient-to-br from-purple-400 to-pink-500 hover:from-pink-500 hover:to-yellow-500 rounded-xl font-bold text-white shadow-2xl">
-        <Branches />
-      </div>
-      <div
-        className="col-span-5 row-span-2 col-start-1 row-start-2 text-center items-center rounded-xl shadow-3xl text-white h-full font-bold"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        Lead Time for Change
-        <LeadTimeChart />
-      </div>
-      <div className="flex col-span-2 col-start-1 row-start-4 h-32 p-2 iflex w-full justify-center items-center bg-gradient-to-br from-lime-400 to-green-500 hover:from-pink-500 hover:to-yellow-500 rounded-xl font-bold text-white shadow-2xl">
-        <MergeSuccessRate />
-      </div>
-      <div
-        className="col-start-3 row-start-4 rounded-xl shadow-3xl ml-1 h-32 p-2 items-center justify-center font-bold text-white"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <MergedPRCount />
-      </div>
-      <div
-        className="col-start-4 row-start-4 rounded-xl shadow-3xl ml-1 h-32 p-2 items-center justify-center font-bold text-white"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <OpenPullRequests />
-      </div>
-      <div
-        className="col-start-3 row-start-5 rounded-xl shadow-3xl ml-1 h-32 p-2 items-center justify-center font-bold text-white"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <ClosedIssues />
-      </div>
-      <div
-        className="col-start-4 row-start-5 rounded-xl shadow-3xl ml-1 h-32 p-2 items-center justify-center font-bold text-white"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <NewIssues />
-      </div>
-      <div
-        className="flex row-span-2 col-start-1 row-start-6 rounded-xl shadow-3xl ml-1 h-68 items-center justify-center font-bold text-white bg-gradient-to-br from-yellow-300 to-orange-500 hover:from-pink-500 hover:to-yellow-500 z-10 transition ease-in-out duration-200 hover:w-80 hover:relative hover:right-24 oveflow-scroll"
-        style={{
-          backgroundColor: "#171C2Eff",
-          transition: "ease-in-out 500ms",
-        }}
-      >
-        <Forks />
-      </div>
-      <div
-        className="col-span-2 col-start-1 row-start-5 rounded-xl shadow-3xl ml-1 h-32 p-2 items-center justify-center font-bold text-white bg-gradient-to-br from-rose-400 to-red-700 hover:from-pink-500 hover:to-yellow-500"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <CommentsPerCodeRatio />
-      </div>
-      <div
-        className="col-span-4 row-span-3 col-start-1 row-start-8 ml-1 h-96 p-2 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <BarChart fullRepo={currRepo} />
-      </div>
-      <div
-        className="col-span-4 row-span-3 col-start-1 row-start-11 p-2 h-96 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <IssuesTimeline />
-      </div>
-      <div
-        className="col-start-6 row-start-2 p-2 h-32 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <NumCommits />
-      </div>
-      <div
-        className="col-start-7 row-start-2 p-2 h-32 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <NumDeployments />
-      </div>
-      <div
-        className="col-span-3 col-start-1 row-start-15 p-2 h-32 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white bg-gradient-to-br from-emerald-500 to-emerald-800"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <Stargazers />
-      </div>
-      <div
-        className="col-span-3 col-start-1 row-start-14 p-2 h-32 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white bg-gradient-to-br from-blue-500 to-blue-900"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <DateCreated />
-      </div>
-      <div
-        className="col-span-4 row-span-3 col-start-1 row-start-16 p-2 h-96 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        Change Failure Rate (CFR)
-      </div>
-      <div
-        className="col-span-4 row-span-6 col-start-5 row-start-19 p-2 h-192 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <Activity />
-      </div>
-      <div
-        className="col-span-4 row-span-3 col-start-1 row-start-25 p-2 h-96 text-center justify-center items-center font-bold rounded-xl shadow-3xl text-white"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <MergeSuccessRatePie />
-      </div> */}
     </div>
   </div>
   );
