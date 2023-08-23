d48af67328405702261ed3e391ea84a30266d45c
jons-repo <s.jonathan4252@gmail.com>
2023-08-22 22:17:13 -0400

    Make list centered and uniformed

--- src/components/NotificationsList.jsx
+++ src/components/NotificationsList.jsx
@@ -20,22 +20,21 @@ const NotificationsList = () => {
   }, [page]);
 
   return (
-    <div>
-      <div className="flex text-3xl text-white p-5 justify-center items-center">
-        <div>Your Notifications</div>
-        <div className="ml-4">
-          <VscBell style={{ color: "green" }} />
+    <div className="flex flex-col items-center">
+      <div className="text-3xl text-white p-5">
+        <div className="flex items-center">
+          <div>Your Notifications</div>
+          <div className="ml-4">
+            <VscBell style={{ color: "green" }} />
+          </div>
         </div>
       </div>
-      <div style={{ backgroundColor: "#171C2Eff" }}>
-        <ul
-          className="h-full w-fit text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white "
-          style={{ backgroundColor: "#171C2Eff" }}
-        >
+      <div className="w-full bg-white rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+        <ul className="text-sm font-medium">
           {notifications.map((notification, index) => (
             <li
               key={index}
-              className="w-1/3 py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white hover:bg-gradient-to-br from-lime-400 to-green-500 hover:text-black"
+              className="py-2 pl-4 border-b border-gray-200 dark:border-gray-600 text-center text-white hover:bg-gradient-to-br from-lime-400 to-green-500 hover:text-black"
             >
               {notification.type} - {notification.title} - {notification.timeAgo}
             </li>
