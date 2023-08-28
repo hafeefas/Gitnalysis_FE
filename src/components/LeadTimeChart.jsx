e62ade6cbf785f2131eebe23541ba098417e53db
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-08-27 22:59:03 -0400

    Add tooltip for leadtimechange metric

--- src/components/LeadTimeChart.jsx
+++ src/components/LeadTimeChart.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { darkScrollbar, useMediaQuery } from "@mui/material";
 import * as d3 from "d3";
 import { ResponsiveLine } from "@nivo/line";
+import { Tooltip } from '@mui/material';
 
 const LeadTimeChart = () => {
   const currRepo = useSelector((state) => state.repo.currRepo);
@@ -22,6 +23,10 @@ const LeadTimeChart = () => {
   //     },
   //   ];
   // };
+
+  const tooltipContent = () => {
+   return "The lead time metric shows how long on average it takes to merge a PR. It indicates how long senior team members take to review a PR. A longer lead time can mean the senior members are complacent in reviewing the code. This metric may vary depending on the team structure."
+    };
 
   const monthsInSecs = 30 * 24 * 3600;
   const weeksInSecs = 24 * 3600 * 7;
@@ -200,7 +205,10 @@ const LeadTimeChart = () => {
   }, []);
 
   return (
+    <Tooltip title={tooltipContent()} arrow 
+    placement="top">
     <div className="flex w-full justify-center items-center h-56">
+            
       {repoInfo ? (
         <>
           <ResponsiveLine
@@ -361,6 +369,7 @@ const LeadTimeChart = () => {
         </div>
       )}
     </div>
+  </Tooltip>
   );
 };
 
