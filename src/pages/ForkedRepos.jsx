b3fe61f21a76d90f2bb16d6a59e71d4047e9fcec
Ghulam Nabi <ghualmnabia75@gmail.com>
2023-08-28 00:21:54 -0400

    Starred repo, Forked repo and Owned repo page have same UI

--- src/pages/ForkedRepos.jsx
+++ src/pages/ForkedRepos.jsx
@@ -5,6 +5,7 @@ import { setCurrentRepo } from "../redux/slices/repoSlice";
 import { FaCodeFork } from "react-icons/fa6";
 import { useMediaQuery } from "@mui/material";
 import { getForkedRepos } from "../redux/slices/repoSlice";
+import { motion } from "framer-motion"
 
 
 const ForkedRepos = () =>{
@@ -109,58 +110,154 @@ const ForkedRepos = () =>{
         }
 
     return(
-        <div className="flex justify-center items-center p-4">
-            <div className="w-96 border shadow-xl rounded-md p-4 py-6 px-4 my-2 mb-2 border-black flex-col justify-center items-center"
-                style={{ backgroundColor: "#171C2Eff" }}>
-                <div className="flex h-fit text-4xl text-white font-serif p-4 justify-center rounded-lg shadow-lg  items-center border-black">
-                    <h1>Forked Repos</h1>
-                    <div className="ml-4">
-                        <FaCodeFork style={{ color: "gray" }} />
-                    </div>
-                </div>
-                <div className="flex justify-center items-center" style={{ backgroundColor: "#171C2Eff" }}>
-                    <input
-                        type="text"
-                        placeholder="Search for forked repos..."
-                        className="mb-4 py-2 px-4 border border-black rounded-md text-gray-900 bg-white border-gray-200 rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white shadow-lg"
-                        style={{ width: "300px" }}
-                        value={forkedReposSearchQuery}
-                        onChange={handleForkedReposInputChange}
-                    />{showDropdown && (
-                        <ul
-                          className="h-16 w-64 overflow-y-scroll text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white "
-                          style={{ backgroundColor: "#171C2Eff" }}
-                        >
-                          {filterForked.length > 0 ? (
-                    filterForked.map((repoName) => (
-                        <li
-                        key={repoName.full_name}
-                        onClick={() => handleDropdownItemClick(repoName.full_name)}
-                        className="w-full rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white hover:bg-gradient-to-br from-teal-300 hover:to-sky-500 hover:text-black"
-                        >
-                        {repoName.name}
-                        </li>
-                    ))
-                    ) : (
-                    <li className="w-full rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white bg-gradient-to-br from-red-300 to-red-900">
-                        No Results
-                    </li>
-                    )}
-                        </ul>
-                      )}
-                </div>
-                <ul className="h-fit w-fit text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white" style={{ backgroundColor: "#171C2Eff" }}>
-                    {filterForked?.map((repoName) => (
-                        <li
-                            key={repoName.full_name}
-                            onClick={() => handleClickRepo(repoName.full_name)}
-                            className="w-full rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white hover:bg-gradient-to-br from-teal-300 hover:to-sky-500 hover:text-black">
-                            {repoName.name}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
+        // <div className="flex-col flex justify-center items-center p-4">
+        //     <div className="w-96 border shadow-xl rounded-md p-4 py-6 px-4 my-2 mb-2 border-black flex-col justify-center items-center"
+        //         style={{ backgroundColor: "#171C2Eff" }}>
+        //         <div className="flex h-fit text-4xl text-white font-serif p-4 justify-center rounded-lg shadow-lg  items-center border-black">
+        //             <h1>Forked Repos</h1>
+        //             <div className="ml-4">
+        //                 <FaCodeFork style={{ color: "gray" }} />
+        //             </div>
+        //         </div>
+        //         <div className="flex justify-center items-center" style={{ backgroundColor: "#171C2Eff" }}>
+        //             <input
+        //                 type="text"
+        //                 placeholder="Search for forked repos..."
+        //                 className="mb-4 py-2 px-4 border border-black rounded-md text-gray-900 bg-white border-gray-200 rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white shadow-lg"
+        //                 style={{ width: "300px" }}
+        //                 value={forkedReposSearchQuery}
+        //                 onChange={handleForkedReposInputChange}
+        //             />{showDropdown && (
+        //                 <ul
+        //                   className="h-16 w-64 overflow-y-scroll text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white "
+        //                   style={{ backgroundColor: "#171C2Eff" }}
+        //                 >
+        //                   {filterForked.length > 0 ? (
+        //             filterForked.map((repoName) => (
+        //                 <li
+        //                 key={repoName.full_name}
+        //                 onClick={() => handleDropdownItemClick(repoName.full_name)}
+        //                 className="w-full rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white hover:bg-gradient-to-br from-teal-300 hover:to-sky-500 hover:text-black"
+        //                 >
+        //                 {repoName.name}
+        //                 </li>
+        //             ))
+        //             ) : (
+        //             <li className="w-full rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white bg-gradient-to-br from-red-300 to-red-900">
+        //                 No Results
+        //             </li>
+        //             )}
+        //                 </ul>
+        //               )}
+        //         </div>
+        //         <ul className="h-fit w-fit text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white" style={{ backgroundColor: "#171C2Eff" }}>
+        //             {filterForked?.map((repoName) => (
+        //                 <li
+        //                     key={repoName.full_name}
+        //                     onClick={() => handleClickRepo(repoName.full_name)}
+        //                     className="w-full rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white hover:bg-gradient-to-br from-teal-300 hover:to-sky-500 hover:text-black">
+        //                     {repoName.name}
+        //                 </li>
+        //             ))}
+        //         </ul>
+        //     </div>
+        //     <ul className="h-fit w-fit text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white" style={{ backgroundColor: "#171C2Eff" }}>
+        //             {filterForked?.map((repoName) => (
+        //                 <li
+        //                     key={repoName.full_name}
+        //                     onClick={() => handleClickRepo(repoName.full_name)}
+        //                     className="w-full rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white hover:bg-gradient-to-br from-teal-300 hover:to-sky-500 hover:text-black">
+        //                     {repoName.name}
+        //                 </li>
+        //             ))}
+        //         </ul>
+        // </div>
+        <div className="flex-col flex justify-center items-center p-4">
+      <div className="duration-300 w-96 border shadow-xl rounded-md p-4 py-6 px-4 my-2 mb-2 border-black flex-col justify-center items-center"
+            style={{ backgroundColor: "#171C2Eff" }}>
+        <div className="flex h-fit text-4xl text-white font-serif p-4 justify-center rounded-lg shadow-lg  items-center border-black">
+              <h1>Forked Repos</h1>
+              <div className="ml-4">
+                  <FaCodeFork style={{ color: "green" }} />
+              </div>
+          </div>
+        <div
+          className="grid justify-items-center"
+          style={{ backgroundColor: "#171C2Eff" }}
+        >
+          <input
+            type="text"
+            placeholder="Search for forked repos..."
+            className="mb-4 mt-8 py-2 px-4 border border-black rounded-md text-gray-900 bg-white border-gray-200 rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white shadow-lg"
+            style={{ width: "300px" }}
+            value={searchQuery}
+            onChange={handleInputChange}
+          />
+          {showDropdown && (
+            <motion.ul
+            variants={{
+              hidden: {opacity: 0, x: -80},
+              visible: {opacity: 1, x: 0},
+            }}
+            initial="hidden"
+            animate="visible"
+            transition={{duration: 0.8, delay: 0.25}}
+            className="h-fit w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+              style={{ backgroundColor: "#171C2Eff" }}
+            >
+              {filteredRepos.length > 0 ? (
+                filteredRepos.map((repoName) => (
+                  <motion.li
+                  variants={{
+                    hidden: {opacity: 0, x: 80},
+                    visible: {opacity: 1, x: 0},
+                  }}
+                  initial="hidden"
+                  animate="visible"
+                  transition={{duration: 0.8, delay: 0.50}}
+                    key={repoName.full_name}
+                    onClick={() => handleDropdownItemClick(repoName.full_name)}
+                    className="w-48 rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white hover:bg-gradient-to-br from-teal-300 hover:to-sky-500 hover:text-black"
+                  >
+                    {repoName.name}
+                  </motion.li>
+                ))
+              ) : (
+                <li className="w-48 rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white bg-gradient-to-br from-red-300 to-red-900">
+                  No Results
+                </li>
+              )}
+            </motion.ul>
+          )}
+
+          
+        </div>  
+      </div>
+      <motion.div
+      variants={{
+        hidden: {opacity: 0, x: -80},
+        visible: {opacity: 1, x: 0},
+      }}
+      initial="hidden"
+      animate="visible"
+      transition={{duration: 0.8, delay: 0.75}}
+       className="mt-10" style={{ backgroundColor: "#171C2Eff" }}>
+        <ul
+          className="h-fit ml-5 mr-5 w-fit text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white "
+          style={{ backgroundColor: "#171C2Eff" }}
+        >
+          {sortedForkedRepos?.map((repo) => (
+            <li
+              key={repo.full_name}
+              onClick={() => handleClickRepo(repo.full_name)}
+              className="w-full rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white hover:bg-gradient-to-br from-teal-300 hover:to-sky-500 hover:text-black"
+            >
+              {repo.name}
+            </li>
+          ))}
+        </ul>
+      </motion.div>
+    </div>
         
     )
 }
