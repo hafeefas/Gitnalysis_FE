9cf1e354abe8bc6b8c1ac3f36a9d0a920fdfc5a1
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-08-31 04:21:13 -0400

    scroll impelemtnation

--- src/pages/OwnedRepos.jsx
+++ src/pages/OwnedRepos.jsx
@@ -72,103 +72,142 @@ const OwnedRepos = () => {
     fetchUserRepos();
   }, []);
 
-  // if (allRepos) {
-  //   console.log(allRepos);
-  // }
-
   // Check if loggedInUser is available
   if (!loggedInUser) {
     return <div>Please log in to view the repos that you own!</div>; // or render some loading indicator
   }
 
   return (
-    <div className="mt-12 flex-col flex justify-center items-center p-4">
-      <div
-        className="mt-8 duration-300 w-96 border shadow-xl rounded-md p-4 py-6 px-4 my-2 mb-2 border-black flex-col justify-center items-center"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <div className="flex h-fit text-4xl text-white font-serif p-4 justify-center rounded-lg shadow-lg  items-center border-black">
-          <h1>Your Repos</h1>
-          <div className="ml-4">
-            <VscRepo style={{ color: "green" }} />
+    <div className="grid w-full overflow-scroll mt-12 items-center p-4 mb-20">
+      <div className="flex justify-center p-4 overflow-scroll ">
+        <div
+          className="flex mt-8 duration-300 w-96 border shadow-xl rounded-md p-4 py-6 px-4 my-2 mb-2 border-black flex-col justify-center items-center"
+          style={{ backgroundColor: "#171C2Eff" }}
+        >
+          <div className="flex text-4xl text-white font-serif p-4 justify-center rounded-lg shadow-lg  items-center border-black">
+            <h1>Your Repos</h1>
+            <div className="ml-4">
+              <VscRepo style={{ color: "green" }} />
+            </div>
+          </div>
+          <div
+            className="grid justify-items-center"
+            style={{ backgroundColor: "#171C2Eff" }}
+          >
+            <input
+              type="text"
+              placeholder="Search for forked repos..."
+              className="mb-4 mt-8 py-2 px-4 border border-black rounded-md text-gray-900 bg-white dark:bg-gray-700 dark:border-gray-600 dark:text-white shadow-lg"
+              style={{ width: "300px" }}
+              value={searchQuery}
+              onChange={handleInputChange}
+            />
+            {showDropdown && (
+              <motion.ul
+                variants={{
+                  hidden: { opacity: 0, x: -80 },
+                  visible: { opacity: 1, x: 0 },
+                }}
+                initial="hidden"
+                animate="visible"
+                transition={{ duration: 0.8, delay: 0.25 }}
+                className="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                style={{ backgroundColor: "#171C2Eff" }}
+              >
+                {filteredRepos.length > 0 ? (
+                  filteredRepos.map((repoName) => (
+                    <motion.li
+                      variants={{
+                        hidden: { opacity: 0, x: 80 },
+                        visible: { opacity: 1, x: 0 },
+                      }}
+                      initial="hidden"
+                      animate="visible"
+                      transition={{ duration: 0.8, delay: 0.5 }}
+                      key={repoName.full_name}
+                      onClick={() =>
+                        handleDropdownItemClick(repoName.full_name)
+                      }
+                      className="w-48 rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white hover:bg-gradient-to-br from-teal-300 hover:to-sky-500 hover:text-black"
+                    >
+                      {repoName.name}
+                    </motion.li>
+                  ))
+                ) : (
+                  <li className="w-48 rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white bg-gradient-to-br from-red-300 to-red-900">
+                    No Results
+                  </li>
+                )}
+              </motion.ul>
+            )}
           </div>
         </div>
-        <div
-          className="grid justify-items-center"
-          style={{ backgroundColor: "#171C2Eff" }}
-        >
-          <input
-            type="text"
-            placeholder="Search for forked repos..."
-            className="mb-4 mt-8 py-2 px-4 border border-black rounded-md text-gray-900 bg-white border-gray-200 rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white shadow-lg"
-            style={{ width: "300px" }}
-            value={searchQuery}
-            onChange={handleInputChange}
-          />
-          {showDropdown && (
-            <motion.ul
-              variants={{
-                hidden: { opacity: 0, x: -80 },
-                visible: { opacity: 1, x: 0 },
-              }}
-              initial="hidden"
-              animate="visible"
-              transition={{ duration: 0.8, delay: 0.25 }}
-              className="h-fit w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-              style={{ backgroundColor: "#171C2Eff" }}
-            >
-              {filteredRepos.length > 0 ? (
-                filteredRepos.map((repoName) => (
-                  <motion.li
-                    variants={{
-                      hidden: { opacity: 0, x: 80 },
-                      visible: { opacity: 1, x: 0 },
-                    }}
-                    initial="hidden"
-                    animate="visible"
-                    transition={{ duration: 0.8, delay: 0.5 }}
-                    key={repoName.full_name}
-                    onClick={() => handleDropdownItemClick(repoName.full_name)}
-                    className="w-48 rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white hover:bg-gradient-to-br from-teal-300 hover:to-sky-500 hover:text-black"
-                  >
-                    {repoName.name}
-                  </motion.li>
-                ))
-              ) : (
-                <li className="w-48 rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white bg-gradient-to-br from-red-300 to-red-900">
-                  No Results
-                </li>
-              )}
-            </motion.ul>
-          )}
-        </div>
       </div>
-      <motion.div
-        variants={{
-          hidden: { opacity: 0, x: -80 },
-          visible: { opacity: 1, x: 0 },
-        }}
-        initial="hidden"
-        animate="visible"
-        transition={{ duration: 0.8, delay: 0.75 }}
-        className="mt-10"
-        style={{ backgroundColor: "#171C2Eff" }}
-      >
-        <ul
-          className="h-fit ml-5 mr-5 w-fit text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white "
+      <div className="grid grid-cols-2 gap-4">
+        <motion.div
+          variants={{
+            hidden: { opacity: 0, x: -80 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 0.8, delay: 0.75 }}
+          className="mt-10"
           style={{ backgroundColor: "#171C2Eff" }}
         >
-          {sortedOwnerRepos?.map((repo) => (
-            <li
-              key={repo.full_name}
-              onClick={() => handleClickRepo(repo.full_name)}
-              className="w-full rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white hover:bg-gradient-to-br from-lime-400 to-green-500 hover:text-black"
-            >
-              {repo.name}
-            </li>
-          ))}
-        </ul>
-      </motion.div>
+          <ul
+            className="h-fit w-fit text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+            style={{ backgroundColor: "#171C2Eff" }}
+          >
+            {sortedOwnerRepos?.map((repo, index) => {
+              if (index % 2 === 0) {
+                return (
+                  <li
+                    key={repo.full_name}
+                    onClick={() => handleClickRepo(repo.full_name)}
+                    className="w-full rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white hover:bg-gradient-to-br from-lime-400 to-green-500 hover:text-black"
+                  >
+                    {repo.name}
+                  </li>
+                );
+              }
+              return null;
+            })}
+          </ul>
+        </motion.div>
+
+        <motion.div
+          variants={{
+            hidden: { opacity: 0, x: -80 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 0.8, delay: 0.75 }}
+          className="mt-10"
+          style={{ backgroundColor: "#171C2Eff" }}
+        >
+          <ul
+            className="h-fit w-fit text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+            style={{ backgroundColor: "#171C2Eff" }}
+          >
+            {sortedOwnerRepos?.map((repo, index) => {
+              if (index % 2 !== 0) {
+                return (
+                  <li
+                    key={repo.full_name}
+                    onClick={() => handleClickRepo(repo.full_name)}
+                    className="w-full rounded-lg py-2 pl-4 border-b border-gray-200 dark:border-gray-600 inline-block items-center text-white hover:bg-gradient-to-br from-lime-400 to-green-500 hover:text-black"
+                  >
+                    {repo.name}
+                  </li>
+                );
+              }
+              return null;
+            })}
+          </ul>
+        </motion.div>
+      </div>
     </div>
   );
 };
